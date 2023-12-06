da52f6e7acf26a570006c8f2cde6ca9391613c0b
alynch <anthonyvlynch5@gmail.com>
2023-12-06 00:26:03 -0500

    updated WinBanner props to take guessArrayLength

--- src/components/WinBanner/WinBanner.js
+++ src/components/WinBanner/WinBanner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Banner from '../Banner/Banner';
 
-function WinBanner({ guessArray, handleRestart }) {
-  const numOfGuesses = guessArray.length === 1 ?
-    '1 guess' : `${guessArray.length} guesses`;
+function WinBanner({ guessArrayLength, handleRestart }) {
+  const numOfGuesses = guessArrayLength === 1 ?
+    '1 guess' : `${guessArrayLength} guesses`;
   return (
     <Banner
       variant='happy'
