d1b559a5af5e9ab2b73c6bce5e2e93ede2ed6e0f
alynch <anthonyvlynch5@gmail.com>
2024-04-08 13:46:58 -0400

    use words dictionary JSON to validate user inputs. Create new state to handle input error status and inut error styling

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import useSWRImmutable from 'swr/immutable'
+import React from "react";
+import useSWRImmutable from "swr/immutable";
 
-import ImportForm from '../InputForm';
-import GuessResults from '../GuessResults';
-import WinBanner from '../WinBanner';
-import LoseBanner from '../LoseBanner';
-import Keyboard from '../Keyboard';
-import { NUM_OF_GUESSES_ALLOWED, ENDPOINT } from '../../constants';
-import { checkGuess, fetcher } from '../../game-helpers';
+import InputForm from "../InputForm";
+import GuessResults from "../GuessResults";
+import WinBanner from "../WinBanner";
+import LoseBanner from "../LoseBanner";
+import Keyboard from "../Keyboard";
+import { NUM_OF_GUESSES_ALLOWED, ENDPOINT } from "../../constants";
+import { checkGuess, fetcher } from "../../game-helpers";
+import { words } from "../../words_dictionary";
 
 function Game() {
   const [guessArray, setGuessArray] = React.useState([]);
-  const [status, setStatus] = React.useState('in progress');
+  const [status, setStatus] = React.useState("in progress");
+  const [guessError, setGuessError] = React.useState(false);
   // SWR to fetch word and control loading and error states
   const { data, isLoading, error, mutate } = useSWRImmutable(ENDPOINT, fetcher);
   const answer = data ? data[0].toUpperCase() : null;
-  
+
   const handleAddGuess = (guess) => {
-    //check guess before adding to guessArray
-    const checkedGuess=checkGuess(guess, answer);
-    const newGuessArray = [
-      ...guessArray,
-      checkedGuess
-    ];
-    setGuessArray(newGuessArray);
-    if (guess === answer) {
-      setStatus('win');
-    } else if (newGuessArray.length >= NUM_OF_GUESSES_ALLOWED) {
-      setStatus('lose');
+    const validWord = words[guess.toLowerCase()] === 1;
+    setGuessError(!validWord);
+    if (validWord) {
+      setGuessError(false);
+      //check guess before adding to guessArray
+      const checkedGuess = checkGuess(guess, answer);
+      const newGuessArray = [...guessArray, checkedGuess];
+      setGuessArray(newGuessArray);
+      if (guess === answer) {
+        setStatus("win");
+      } else if (newGuessArray.length >= NUM_OF_GUESSES_ALLOWED) {
+        setStatus("lose");
+      }
     }
-  }
+  };
 
   const handleRestart = () => {
     mutate(ENDPOINT);
     setGuessArray([]);
-    setStatus('in progress');
-  }
+    setStatus("in progress");
+  };
 
   if (isLoading) {
     return <p>Loading ...</p>;
@@ -45,11 +49,18 @@ function Game() {
   }
   return (
     <>
-      <GuessResults guessArray={guessArray}/>
-      <ImportForm handleAddGuess={handleAddGuess} status={status}/>
-      <Keyboard guessArray={guessArray}/>
-      {status === "win" && <WinBanner guessArrayLength={guessArray.length} handleRestart={handleRestart}/>}
-      {status === "lose" && <LoseBanner answer={answer} handleRestart={handleRestart}/>}
+      <GuessResults guessArray={guessArray} />
+      <InputForm handleAddGuess={handleAddGuess} status={status} guessError={guessError}/>
+      <Keyboard guessArray={guessArray} />
+      {status === "win" && (
+        <WinBanner
+          guessArrayLength={guessArray.length}
+          handleRestart={handleRestart}
+        />
+      )}
+      {status === "lose" && (
+        <LoseBanner answer={answer} handleRestart={handleRestart} />
+      )}
     </>
   );
 }
