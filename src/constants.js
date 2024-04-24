1cd8ab900ae35c98cc4b27c0ed08d513ca608d86
alynch <anthonyvlynch5@gmail.com>
2024-04-24 14:05:30 -0400

    changed ENDPOINT to WORD_ENDPOINT and added VALID_ENDPOINT for guess validation

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const NUM_OF_GUESSES_ALLOWED = 6;
 export const WORD_LENGTH = 5;
-export const ENDPOINT = `https://random-word-api.herokuapp.com/word?length=${WORD_LENGTH}`;
+export const WORD_ENDPOINT = `https://random-word-api.herokuapp.com/word?length=${WORD_LENGTH}`;
+export const VALID_ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en";
